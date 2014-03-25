90ef07c41a2e6637fd79a0a979234b6e3c3d241a
Ddo <joeddo89@gmail.com>
2014-03-25 18:47:27 +0700

    remove function name

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ var request = require('request');
 describe("#yew", function() {
     describe("#simple", function() {
         it("should correct", function(done) {
-            yew(function *gen() {
+            yew(function *() {
                 var data = yield [request, 'https://graph.facebook.com/facebook'];
 
                 expect(data[0]).to.be.a('null');
@@ -18,7 +18,7 @@ describe("#yew", function() {
 
     describe("#object", function() {
         it("should correct", function(done) {
-            yew(function *gen() {
+            yew(function *() {
                 var data = yield [request, {
                     url: 'https://graph.facebook.com/GitHub',
                     json: true
@@ -34,7 +34,7 @@ describe("#yew", function() {
 
     describe("#request.get", function() {
         it("should correct", function(done) {
-            yew(function *gen() {
+            yew(function *() {
                 var data = yield [request.get, 'https://graph.facebook.com/tjholowaychuk'];
 
                 expect(data[0]).to.be.a('null');
